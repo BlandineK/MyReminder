ef35e4aff562d60057bf9b67c01c6b681b9b0c18
BlandineK <blandineuwase222@gmail.com>
2019-10-14 08:24:27 +0200

    add ButterKnife

--- app/src/main/java/com/moringashool/myreminder/MainActivity.java
+++ app/src/main/java/com/moringashool/myreminder/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
 
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mGetRemindersButton = (Button) findViewById(R.id.GetRemindersButton);
