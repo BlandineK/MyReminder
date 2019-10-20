cfcc6b828cb51f0dc5b238115ac9db74dbaa6466
BlandineK <blandineuwase222@gmail.com>
2019-10-21 01:42:23 +0200

    add new files

--- app/src/main/java/Constants.java
+++ app/src/main/java/Constants.java
@@ -1,2 +1,7 @@
+import com.moringashool.myreminder.BuildConfig;
+
 public class Constants {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+
 }
